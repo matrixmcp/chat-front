d4718294cbc650667bd22fe6f5bd86196598043f
Sergey Konshu <sk.web@mail.ru>
2018-05-20 19:32:56 +0600

    change api root

--- src/__data__/constatnts.js
+++ src/__data__/constatnts.js
@@ -4,7 +4,9 @@ export const STATUSES = {
     'ERROR': 'ERROR',
 }
 
-export const API_ROOT = 'http://localhost:5000'
+export const API_ROOT = process.env.NODE_ENV === 'production' 
+    ? 'https://api-my-chat1.herokuapp.com:5000'
+    :'http://localhost:5000'
 
 export const FORMATS_DATETIME = {
     FULL: 'DD/MM/YYYY HH:mm:ss',
