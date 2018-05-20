80984dfb0c21fc178fd9a722075dcbb68d46b0a8
Sergey Konshu <sk.web@mail.ru>
2018-05-20 20:05:43 +0600

    fix api root

--- src/__data__/constatnts.js
+++ src/__data__/constatnts.js
@@ -6,7 +6,7 @@ export const STATUSES = {
 
 export const API_ROOT = process.env.NODE_ENV === 'production' 
     ? 'https://api-my-chat1.herokuapp.com'
-    : 'http://localhost'
+    : 'http://localhost:5000'
 
 export const FORMATS_DATETIME = {
     FULL: 'DD/MM/YYYY HH:mm:ss',
